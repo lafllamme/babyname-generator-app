460ed06bc2a9f37a3abc6d24f4dfcbb6bf61cfea
Lafllamme <teke900@gmail.com>
2022-11-18 16:47:16 +0100

    initial commit

---
+++ pages/checkThis.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Baby Name Generator</h1>
+    <p>Choose your option and click the "Find Name" button below.</p>
+    <div class="options-container">
+      <div class="option-container">
+        <h4>1)Choose a Gender</h4>
+        <div class="option-buttons">
+          <button>Boy</button>
+          <button>Girl</button>
+          <button>Unisex</button>
+        </div>
+      </div>
+      <div class="option-container">
+        <h4>2) Choose the name's popularity</h4>
+        <div class="options-buttons">
+          <button>Trendy</button>
+        </div>
+      </div>
+      <div class="option-container" />
+      <div class="option-container" />
+    </div>
+  </div>
+</template>
